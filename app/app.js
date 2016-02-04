87a5a38d97802de9923f477cf070a2961e77d57f
apedley <apedley@gmail.com>
2016-02-04 12:24:20 -0600

    Added data to dialog and only showing restaurants

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ String.prototype.capitalize = function(){
     };
 
 var app = {};
-app.queryURL = "https://data.cityofchicago.org/resource/cwig-ma7x.json?$select=dba_name,risk,facility_type,results,violations,aka_name,address,city,state,inspection_date,inspection_type,latitude,longitude"; 
+app.queryURL = "https://data.cityofchicago.org/resource/cwig-ma7x.json?facility_type=Restaurant&$select=dba_name,risk,facility_type,results,violations,aka_name,address,city,state,inspection_date,inspection_type,latitude,longitude&$limit=3000"; 
 
 
 app.init = function() {
@@ -15,14 +15,14 @@ app.init = function() {
                               .attr('height', 1500)
                               .attr('class', 'background');
   $('#dialog').dialog({
-    autoOpen: false
+    autoOpen: false,
+    width: 800
   });
 
   app.tip = d3.tip()
     .attr('class', 'd3-tip')
     .offset([-10, 0])
     .html(function(d) {
-      debugger;
       return "<strong>" + d.dba_name.capitalize() + "</strong><br>" + d.address.capitalize() + "<br><strong>" + d.results + "</strong>";
   });
 
@@ -72,8 +72,35 @@ app.translateLongToX = function(longitude) {
 }
 
 app.showDetails = function(data, index) {
+  $("#dialog").dialog("close");
   var selector = "#datapoint" + index;
+  var name = data.dba_name.capitalize();
+  var address = data.address.capitalize();
+  var city = data.city.capitalize();
+  var date = data.inspection_date;
+  var state = data.state;
+  var type = data.inspection_type;
+  var violations = data.violations || "None listed";
+  violations = violations.capitalize();
+  violations = violations.split('|');
+
+  var result = data.results;
+  
+  var htmlString =  "<ul>";
+
+  htmlString = htmlString + "<li><strong>Result: </strong><span class='" + result.replace(/\s/g, '').replace(/\//g, '') + "'>" + result + "</span></li>";
+
+  htmlString = htmlString + "<li><strong>Address: </strong>" + address + "</li>"
+                  + "<li><strong>Date: </strong>" + date + "</li>"
+                  + "<li><strong>Type: </strong>" + type + "</li>"
+                  + "<li><strong>Violations: </strong><br>" + violations.join("<br><br>") + "</li>"
+                  + "</ul>";
+
+
+  $('#dialog').dialog('option', 'title', name);;
+  $("#dialog").html(htmlString);
   $("#dialog").dialog("open");
+  debugger;
 }
 
 app.drawMap = function(data) {
@@ -89,7 +116,7 @@ app.drawMap = function(data) {
         return app.translateLatitudeToY(parseFloat(d.latitude))+25;
       })
       .attr('r', function(d) {
-        return 5;
+        return 4;
       })
       .style('fill', function(d) {
         if (d.results.capitalize() === 'Pass') {
@@ -114,7 +141,6 @@ app.drawMap = function(data) {
         app.tip.hide();
       })
       .on("click", function(d, i) {
-        console.log("clicked");
         app.showDetails(d, i);
 
       });
